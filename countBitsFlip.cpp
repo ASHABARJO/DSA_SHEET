ba4b1e1f44ccaf331feeaffa0dd65500b1cd600f
ASHA BARJO <89405319+ASHABARJO@users.noreply.github.com>
2023-06-30 00:01:55 +0530

    Create countBitsFlip.cpp

---
+++ countBitsFlip.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    // Function to find number of bits needed to be flipped to convert A to B
+    int countBitsFlip(int a, int b){
+        
+        // Your logic here
+        
+        if(a==b)
+        return 0;
+        int xr=a^b;
+        int cnt=1;
+        while(xr &(xr-1)){
+            cnt++;
+            xr=xr&(xr-1);
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver Code
+int main()
+{
+	int t;
+	cin>>t;// input the testcases
+	while(t--) //while testcases exist
+	{
+		int a,b;
+		cin>>a>>b; //input a and b
+
+        Solution ob;
+		cout<<ob.countBitsFlip(a, b)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
